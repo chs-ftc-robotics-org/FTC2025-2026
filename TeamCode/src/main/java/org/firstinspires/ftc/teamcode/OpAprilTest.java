1f4b4f4f2e3afcdaf46637c3adc93c6c99308508
grekand46 <kluo0113@inst.hcpss.org>
2025-10-22 21:37:53 -0400

    testing: Add AprilTag test

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpAprilTest.java
@@ -0,0 +1,42 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+import org.firstinspires.ftc.robotcore.external.hardware.camera.CameraName;
+import org.firstinspires.ftc.vision.VisionPortal;
+import org.firstinspires.ftc.vision.apriltag.AprilTagDetection;
+import org.firstinspires.ftc.vision.apriltag.AprilTagProcessor;
+
+import java.util.List;
+import java.util.Locale;
+
+@Autonomous(name = "April Tag Test")
+public class OpAprilTest extends LinearOpMode {
+    @Override
+    public void runOpMode() {
+        CameraName camera = hardwareMap.get(CameraName.class, "ctl/misc0");
+        AprilTagProcessor aprilProcessor = AprilTagProcessor.easyCreateWithDefaults();
+        VisionPortal visionPortal = VisionPortal.easyCreateWithDefaults(camera, aprilProcessor);
+
+        waitForStart();
+        while (opModeIsActive()) {
+            List<AprilTagDetection> detectedTags = aprilProcessor.getDetections();
+            if (!detectedTags.isEmpty()) {
+                telemetry.clearAll();
+                for (AprilTagDetection tag : detectedTags) {
+                    telemetry.addData("Detected april tag:", displayTagInfo(tag));
+                }
+                telemetry.update();
+            }
+        }
+
+    }
+
+    private static String displayTagInfo(AprilTagDetection tag) {
+        return String.format(
+                Locale.ENGLISH,
+                "{ id: %d, pos: (%f, %f) }",
+                tag.id, tag.center.x, tag.center.y);
+    }
+}
