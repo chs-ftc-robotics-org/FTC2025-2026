b0f4f2a397d8ce834a6bdc5b9498d724793f975b
grekand46 <kluo0113@inst.hcpss.org>
2025-11-22 15:20:05 -0500

    testing: Add servo test

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpServoTest.java
@@ -0,0 +1,24 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+@TeleOp(name = "Servo Test")
+public class OpServoTest extends LinearOpMode {
+    public void runOpMode() {
+        Servo servo = hardwareMap.servo.get("ctl/servo1");
+        waitForStart();
+        while (opModeIsActive()) {
+            if (gamepad1.x) {
+                servo.setPosition(0.15);
+            }
+            if (gamepad1.y) {
+                servo.setPosition(1.0);
+            }
+        }
+
+    }
+}
