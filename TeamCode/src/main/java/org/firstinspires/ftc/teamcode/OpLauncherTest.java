898fb7a77381cb8a1f882dc0b9d53bcb751a7614
grekand46 <kluo0113@inst.hcpss.org>
2025-11-22 15:17:59 -0500

    testing: Disable old launcher test

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpLauncherTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpLauncherTest.java
@@ -1,28 +1,51 @@
 package org.firstinspires.ftc.teamcode;
 
-import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
-import com.qualcomm.robotcore.util.ElapsedTime;
+import com.qualcomm.robotcore.hardware.Servo;
 
 @SuppressWarnings("unused")
-@Autonomous(name = "Launcher Test")
+// @TeleOp(name = "Launcher Test")
 public class OpLauncherTest extends LinearOpMode {
+    DcMotor lower;
+    DcMotor upper;
+    private Servo inputFeed;
+
     @Override
     public void runOpMode() {
-        DcMotor motorA = hardwareMap.get(DcMotor.class, "ctl/motor0");
-        DcMotor motorB = hardwareMap.get(DcMotor.class, "ctl/motor1");
-        ElapsedTime timer = new ElapsedTime();
+        inputFeed = hardwareMap.servo.get("ctl/servo1");
+
+        lower = hardwareMap.get(DcMotor.class, "ctl/motor0");
+        upper = hardwareMap.get(DcMotor.class, "ctl/motor1");
+
         waitForStart();
-        timer.reset();
-        motorA.setPower(1.0);
-        motorB.setPower(-1.0);
         while (opModeIsActive()) {
-            if (timer.milliseconds() > 1000) {
-                break;
+
+            if (gamepad1.left_bumper) {
+                launch();
+            } else {
+                stopEverything();
+            }
+
+            if (gamepad1.x) {
+                inputFeed.setPosition(0.15);
+            }
+            if (gamepad1.y) {
+                inputFeed.setPosition(1.0);
             }
         }
-        motorA.setPower(0.0);
-        motorB.setPower(0.0);
+        lower.setPower(0.0);
+        upper.setPower(0.0);
+    }
+
+    private void launch() {
+        lower.setPower(-0.8);
+        upper.setPower(-0.6);
+    }
+
+    private void stopEverything() {
+        lower.setPower(0.0);
+        upper.setPower(0.0);
     }
 }
