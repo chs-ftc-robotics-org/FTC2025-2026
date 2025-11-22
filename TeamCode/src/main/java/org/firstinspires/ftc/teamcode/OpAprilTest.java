e8e05ee607c16b03f5db5250646cf703d6ba973c
grekand46 <kluo0113@inst.hcpss.org>
2025-11-22 15:16:33 -0500

    testing: Some Limelight only stuff

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpAprilTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpAprilTest.java
@@ -1,9 +1,15 @@
 package org.firstinspires.ftc.teamcode;
 
+import com.qualcomm.hardware.limelightvision.LLResult;
+import com.qualcomm.hardware.limelightvision.LLResultTypes;
+import com.qualcomm.hardware.limelightvision.Limelight3A;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 
 import org.firstinspires.ftc.robotcore.external.hardware.camera.CameraName;
+import org.firstinspires.ftc.robotcore.external.navigation.Pose3D;
+import org.firstinspires.ftc.robotcore.external.navigation.Position;
+import org.firstinspires.ftc.robotcore.external.navigation.YawPitchRollAngles;
 import org.firstinspires.ftc.vision.VisionPortal;
 import org.firstinspires.ftc.vision.apriltag.AprilTagDetection;
 import org.firstinspires.ftc.vision.apriltag.AprilTagProcessor;
@@ -14,24 +20,49 @@ import java.util.Locale;
 @SuppressWarnings("unused")
 @Autonomous(name = "April Tag Test")
 public class OpAprilTest extends LinearOpMode {
+    private Limelight3A webcam;
+
     @Override
     public void runOpMode() {
-        CameraName camera = hardwareMap.get(CameraName.class, "ctl/misc0");
-        AprilTagProcessor aprilProcessor = AprilTagProcessor.easyCreateWithDefaults();
-        @SuppressWarnings("unused")
-        VisionPortal visionPortal = VisionPortal.easyCreateWithDefaults(camera, aprilProcessor);
+        webcam = hardwareMap.get(Limelight3A.class, "webcam");
+        
+        // AprilTagProcessor aprilProcessor = AprilTagProcessor.easyCreateWithDefaults();
+        // @SuppressWarnings("unused")
+        // VisionPortal visionPortal = VisionPortal.easyCreateWithDefaults(camera, aprilProcessor);
+
+        telemetry.setMsTransmissionInterval(11);
+
+        webcam.pipelineSwitch(0);
+
+        webcam.start();
 
         waitForStart();
         while (opModeIsActive()) {
-            List<AprilTagDetection> detectedTags = aprilProcessor.getDetections();
-            if (!detectedTags.isEmpty()) {
-                telemetry.clearAll();
-                for (AprilTagDetection tag : detectedTags) {
-                    telemetry.addData("Detected april tag:", displayTagInfo(tag));
+            LLResult result = webcam.getLatestResult();
+
+            if (result != null) {
+                if (result.isValid()) {
+                    for (LLResultTypes.FiducialResult r : result.getFiducialResults()) {
+                        telemetry.addLine(displayTagInfo(r));
+                    }
+                    telemetry.update();
                 }
-                telemetry.update();
             }
         }
+
+
+
+//        waitForStart();
+//        while (opModeIsActive()) {
+//            List<AprilTagDetection> detectedTags = aprilProcessor.getDetections();
+//            if (!detectedTags.isEmpty()) {
+//                telemetry.clearAll();
+//                for (AprilTagDetection tag : detectedTags) {
+//                    telemetry.addData("Detected april tag:", displayTagInfo(tag));
+//                }
+//                telemetry.update();
+//            }
+//        }
 
     }
 
@@ -40,5 +71,16 @@ public class OpAprilTest extends LinearOpMode {
                 Locale.ENGLISH,
                 "{ id: %d, pos: (%f, %f) }",
                 tag.id, tag.center.x, tag.center.y);
+    }
+
+    private static String displayTagInfo(LLResultTypes.FiducialResult result) {
+        Pose3D pose = result.getTargetPoseCameraSpace();
+        Position p = pose.getPosition();
+        YawPitchRollAngles orientation = pose.getOrientation();
+        return String.format(
+                Locale.ENGLISH,
+                "{ id: %d, pos: (%f, %f) }",
+                result.getFiducialId(), p.x, p.y
+        );
     }
 }
