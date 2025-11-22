9fee7dc0e05f6886ee03c6e41f9256dcd7343ea2
grekand46 <kluo0113@inst.hcpss.org>
2025-11-22 15:18:52 -0500

    testing: Test encoders in motor test

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpMotorTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpMotorTest.java
@@ -13,8 +13,23 @@ public class OpMotorTest extends LinearOpMode {
         waitForStart();
         telemetry.addData("Note", "Test motor power with left stick X of gamepad 1");
         telemetry.update();
+
+
+
+        boolean pressed = false;
+        int target = 0;
+
         while (opModeIsActive()) {
-            motor.setPower(gamepad1.left_stick_x);
+            // motor.setPower(gamepad1.left_stick_x);
+
+            motor.setPower(0.7);
+
+            if (!pressed && gamepad1.a) {
+                motor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+                motor.setTargetPosition(2000);
+                motor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+            }
+            pressed = gamepad1.a;
         }
     }
 }
