00dd7c8102a43028184d0df46b8213e573038236
grekand46 <kluo0113@inst.hcpss.org>
2025-12-08 19:02:52 -0500

    misc: Add documentation for Box utility class

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Box.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Box.java
@@ -1,5 +1,9 @@
 package org.firstinspires.ftc.teamcode;
 
+/// `Box<T>` wraps another object and provides methods to inspect and mutate it
+/// - It is mainly used in `Task` implementations to allow lambdas
+/// to effectively mutate local variables
+/// - Examples are available in the inner `Examples` class
 public class Box<T> {
     private T t;
 
@@ -15,7 +19,34 @@ public class Box<T> {
         this.t = t;
     }
 
+    /// Static factory method to create a `Box`
     public static <T> Box<T> of(T t) {
         return new Box<>(t);
+    }
+
+    @SuppressWarnings("unused")
+    public static class Examples {
+        static void simple() {
+            // Use this to create a `Box` instead of the constructor
+            Box<Integer> num = Box.of(3);
+            assert num.get() == 3;
+
+            num.set(num.get() + 30);
+            assert num.get() == 33;
+        }
+
+        static void lambda() {
+            Box<Integer> num = Box.of(0);
+
+            Runnable f = () -> {
+                System.out.println(num.get());
+
+                // Java does NOT allow directly setting a local variable with `=` in a lambda
+                num.set(num.get() + 1);
+            };
+
+            f.run(); // prints 0
+            f.run(); // prints 1
+        }
     }
 }
