e26e56e94dbfe1f657149004f4b21d820f37d5d1
grekand46 <kluo0113@inst.hcpss.org>
2025-12-13 00:14:09 -0500

    odometry: Take into account the initial position and orientation

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Odometry.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Odometry.java
@@ -6,10 +6,16 @@ import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import org.firstinspires.ftc.robotcore.external.Telemetry;
 import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
 import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.UnnormalizedAngleUnit;
 
 public class Odometry {
     private final GoBildaPinpointDriver driver;
     private final Telemetry telemetry;
+
+    private double startingDegrees;
+
+
+    public static final DistanceUnit UNIT = DistanceUnit.INCH;
 
     public Odometry(OpMode opMode) {
         driver = opMode.hardwareMap.get(GoBildaPinpointDriver.class, "odometry");
@@ -18,8 +24,15 @@ public class Odometry {
         telemetry.update();
     }
 
-    public void reset() {
+    public void resetPos(double x, double y) {
+        driver.setPosX(y, DistanceUnit.INCH);
+        driver.setPosY(x, DistanceUnit.INCH);
+    }
+
+    public void reset(double x, double y, double direction) {
         driver.resetPosAndIMU();
+        startingDegrees = direction;
+        resetPos(x, y);
     }
 
     public void update() {
@@ -28,20 +41,28 @@ public class Odometry {
 
     // These two are swapped because someone put it in the wrong direction
     public double posX() {
-        return driver.getPosY(DistanceUnit.INCH);
+        return driver.getPosY(UNIT);
+        // return driver.getEncoderY();
     }
 
     public double posY() {
-
-        return driver.getPosX(DistanceUnit.INCH);
+        return driver.getPosX(UNIT);
+        // return driver.getEncoderX();
     }
 
-    public double angle() {
-        return driver.getHeading(AngleUnit.DEGREES);
+    public double directionRaw() {
+        double rawAngle = driver.getHeading(AngleUnit.DEGREES);
+        return rawAngle - startingDegrees;
+    }
+
+    public double directionNormalized() {
+        double raw = directionRaw();
+        double result = Util.rem(raw, 360);
+        return result > 180 ? result - 360 : result;
     }
 
     public void log() {
         telemetry.addData("Position", "(%f, %f)", posX(), posY());
-        telemetry.addData("Orientation", angle());
+        telemetry.addData("Orientation", directionNormalized());
     }
 }
